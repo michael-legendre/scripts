e790dcf270f0b8e8b289297e00b6b578b9f15457
michael-legendre <60750387+michael-legendre@users.noreply.github.com>
2020-08-21 15:33:05 -0400

    Update ranks query.sql

--- ranks query.sql
+++ ranks query.sql
@@ -1,10 +1,14 @@
-##creating class ranks and class percentile ranks for Pharm.D./PA programs 
-Create table new_ranks as(
-	Select stunum, gpa, pgmdesc, 
-rank() over (partition by pgmdesc, class order by gpa desc) as class_rank, 
-advisors_by_class.class as class, 
-percent_rank() over (partition by pgmdesc, class order by gpa) as percent_rank
-from ranks
-inner join advisors_by_class on advisors_by_class.studentid = ranks.stunum
-)
-	
+##creating class ranks and class percentile ranks for Pharm.D students 
+Create table ranks as(
+
+select directory.stunum, 
+directory.studentname,
+spring_grades_2020.gpa,
+spring_grades_2020.pgmdesc,
+directory.graddate,
+rank() over (partition by directory.graddate, spring_grades_2020.pgmdesc order by gpa desc) as class_rank,
+percent_rank() over (partition by directory.graddate, spring_grades_2020.pgmdesc order by gpa) as percentile_rank
+from directory 
+join spring_grades_2020 on spring_grades_2020.stunum = directory.stunum
+where directory.graddate = '6/26/2022' or directory.graddate ='6/27/2021' 
+and spring_grades_2020.pgmdesc = 'Doctor of Pharmacy')
